ec01396a14fb08152639ce3eaca5c1c14794b879
Cesar Bassani <cesarbassani@Cesars-MacBook-Pro.local>
2019-05-26 22:24:28 -0400

    Obtendo um post por id

---
+++ src/main/java/com/cesarbassani/workshopmongo/services/PostService.java
@@ -0,0 +1,21 @@
+package com.cesarbassani.workshopmongo.services;
+
+import com.cesarbassani.workshopmongo.domain.Post;
+import com.cesarbassani.workshopmongo.repository.PostRepository;
+import com.cesarbassani.workshopmongo.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    public Post findById(String id) {
+        Optional<Post> obj = postRepository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+}
