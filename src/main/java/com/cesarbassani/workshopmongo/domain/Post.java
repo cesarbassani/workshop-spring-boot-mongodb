cad7ad9c69bb60842b9cc6c92396fa917b088281
Cesar Bassani <cesarbassani@Cesars-MacBook-Pro.local>
2019-05-26 22:37:56 -0400

    Acrescentando comentarios aos posts

--- src/main/java/com/cesarbassani/workshopmongo/domain/Post.java
+++ src/main/java/com/cesarbassani/workshopmongo/domain/Post.java
@@ -1,11 +1,14 @@
 package com.cesarbassani.workshopmongo.domain;
 
 import com.cesarbassani.workshopmongo.dto.AuthorDTO;
+import com.cesarbassani.workshopmongo.dto.CommentDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 @Document(collection = "post")
@@ -20,6 +23,8 @@ public class Post implements Serializable {
     private String body;
 
     private AuthorDTO author;
+
+    private List<CommentDTO> coments = new ArrayList<>();
 
     public Post() {
     }
@@ -70,6 +75,14 @@ public class Post implements Serializable {
 
     public void setAuthor(AuthorDTO author) {
         this.author = author;
+    }
+
+    public List<CommentDTO> getComents() {
+        return coments;
+    }
+
+    public void setComents(List<CommentDTO> coments) {
+        this.coments = coments;
     }
 
     @Override
